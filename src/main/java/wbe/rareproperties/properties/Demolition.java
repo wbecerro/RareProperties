e3e78e2e247e5df4c84c85132dca5c43d4aeecff
wbe <34304532+wbecerro@users.noreply.github.com>
2024-08-01 23:29:54 +0200

    Añadido a la propiedad Demolición un efecto de bloques saltarines y empuje a las entidades afectadas.

--- src/main/java/wbe/rareproperties/properties/Demolition.java
+++ src/main/java/wbe/rareproperties/properties/Demolition.java
@@ -1,12 +1,15 @@
 package wbe.rareproperties.properties;
 
-import org.bukkit.Sound;
+import org.bukkit.*;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.util.Vector;
 import wbe.rareproperties.RareProperties;
 
 import java.util.ArrayList;
@@ -25,11 +28,41 @@ public class Demolition extends RareProperty {
 
         int radius = getConfig().getInt("Properties.Demolition.baseRange") + getLevel();
         int damage = getConfig().getInt("Properties.Demolition.baseDamage") + 2 * getLevel();
+
+        for(Location loc : getCircle(player.getLocation(),2,(2*((int)(Math.PI*2))))){
+            Block block = loc.clone().subtract(0, 1, 0).getBlock();
+            if(block.getType() == Material.AIR) {
+                block = loc.clone().subtract(0, 2, 0).getBlock();
+            }
+            FallingBlock fb = loc.getWorld().spawnFallingBlock(loc, block.getBlockData());
+            fb.setCancelDrop(true);
+            fb.setVelocity(new Vector(0,0.25,0));
+        }
+
+        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(getPlugin(), new Runnable() {
+            @Override
+            public void run() {
+                for(Location loc : getCircle(player.getLocation(),3,(3*((int)(Math.PI*2))))){
+                    Block block = loc.clone().subtract(0, 1, 0).getBlock();
+                    if(block.getType() == Material.AIR) {
+                        block = loc.clone().subtract(0, 2, 0).getBlock();
+                    }
+                    FallingBlock fb = loc.getWorld().spawnFallingBlock(loc, block.getBlockData());
+                    fb.setCancelDrop(true);
+                    fb.setVelocity(new Vector(0,0.35,0));
+                }
+            }
+        }, 2L);
+
         List<Entity> entities = player.getNearbyEntities(radius, radius, radius);
         entities.removeIf(n -> (n instanceof Player));
+        Vector playerVector = player.getLocation().toVector();
         for(Entity entity : entities) {
             if(entity instanceof LivingEntity) {
                 ((LivingEntity) entity).damage(damage);
+                Vector unitVector = entity.getLocation().toVector().subtract(playerVector).normalize();
+                unitVector.setY(0.55);
+                entity.setVelocity(unitVector.multiply(1));
             }
         }
 
@@ -64,5 +97,18 @@ public class Demolition extends RareProperty {
 
         setLevel(level);
         return true;
+    }
+
+    private ArrayList<Location> getCircle(Location center, double radius, int amount){
+        World world = center.getWorld();
+        double increment = ((2 * Math.PI) / amount);
+        ArrayList<Location> locations = new ArrayList<Location>();
+        for(int i = 0;i < amount; i++){
+            double angle = i * increment;
+            double x = center.getX() + (radius * Math.cos(angle));
+            double z = center.getZ() + (radius * Math.sin(angle));
+            locations.add(new Location(world, x, center.getY(), z));
+        }
+        return locations;
     }
 }
